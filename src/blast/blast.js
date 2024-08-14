57212067292041ac51ffa29d8a3102ec17c13bb0
navduhan <duhan27dec@gmail.com>
2024-08-14 15:33:24 -0500

    update

--- src/blast/blast.js
+++ src/blast/blast.js
@@ -4,39 +4,40 @@ const { execSync } = require('child_process');
 
 function parseBlastJSON (file){
 
-    let results ={}
+    let results =[]
 
     // Get the hits from the JSON
     const hits = file.BlastOutput2.report.results.search.hits;
     const query = file.BlastOutput2.report.results.search.query_title;
 
     hits.forEach(hit => {
-
+        
+        let result={}
 
         const qlen = hit.len
         const hsp = hit.hsps[0];
         const hitDesc = hit.description[0]
-        results['query_title']=query
-        results['hitnum']= hit.num;
-        results['hitDesc'] = hitDesc.accession; 
-        results['hitTitle'] = hitDesc.title; 
-        results['identity'] = hsp.identity;
-        results['align_length'] = hsp.align_len;
-        results['gaps'] = hsp.gaps
-        results['mismatch']= hsp.align_len-hsp.identity-hsp.gaps;
-        results['query_start'] = hsp.query_from;
-        results['query_end'] = hsp.query_to;
-        results['subject_start'] = hsp.hit_from;
-        results['subject_end'] = hsp.hit_to;
-        results['query_strand'] = hsp.query_strand;
-        results['subject_strand'] = hsp.hit_strand;
-        results['score'] = hsp.score;
-        results['evalue'] = hsp.evalue;
-        results['bit_score'] = hsp.bit_score;
-        results['qseq']= hsp.qseq;
-        results['sseq']= hsp.hseq;
-        results['midline']= hsp.midline;
-
+        result['query_title']=query
+        result['hitnum']= hit.num
+        result['hitDesc'] = hitDesc.accession; 
+        result['hitTitle'] = hitDesc.title; 
+        result['identity'] = (hsp.identity/hsp.align_len*100).toFixed(2);
+        result['align_length'] = hsp.align_len;
+        result['gaps'] = hsp.gaps
+        result['mismatch']= hsp.align_len-hsp.identity-hsp.gaps;
+        result['query_start'] = hsp.query_from;
+        result['query_end'] = hsp.query_to;
+        result['subject_start'] = hsp.hit_from;
+        result['subject_end'] = hsp.hit_to;
+        result['query_strand'] = hsp.query_strand;
+        result['subject_strand'] = hsp.hit_strand;
+        result['score'] = hsp.score;
+        result['evalue'] = hsp.evalue;
+        result['bit_score'] = hsp.bit_score;
+        result['qseq']= hsp.qseq;
+        result['sseq']= hsp.hseq;
+        result['midline']= hsp.midline;
+        results.push(result)
 
     })
 
